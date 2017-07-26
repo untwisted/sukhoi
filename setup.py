ce67f0a8f9c310d575a54630b4386f6625d9b8df
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2017-07-25 21:04:33 -0300

    Fixing setup.py version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 setup(name="sukhoi",
-      version="0.0.6",
+      version="1.0.0",
       py_modules=["sukhoi"],
       author="Iury O. G. Figueiredo",
       author_email="ioliveira@id.uff.br",
@@ -11,6 +11,7 @@ setup(name="sukhoi",
       keywords=['web', 'crawler', 'extract', 'parse', 'html'],
       classifiers=[],
       description="Minimalist and powerful Web Crawler.",)
+
 
 
 
